225e9f35b312aa6926c8497808e83f0b59c611fb
William <william.directinsight.co.uk>
2017-08-31 17:16:23 +0100

    Added command line args

--- enc/enc.cpp
+++ enc/enc.cpp
@@ -4,19 +4,43 @@
 
 #pragma warning(disable : 4996)
 
-int main()
+#define LF 10
+#define HT 9
+
+int main(int argc, char *argv[])
 
 {
-	int character = NULL; // character
-	int seed = 1; // encodein seed valid range 1 to 26
-	int mode = 0;  // 1 = encode, 0 = decode
-	// int f = 0; // for decode indicatred found valid character, no additional processing required in the next test
+	char character = NULL; // character
+	int seed = 15; // encodein seed valid range 1 to 127
+	int mode = 1; // 1 = encode, 0 = decode
 
 	FILE *inFile;
 	FILE *outFile;
 
-	inFile = fopen("inFile.txt", "r");
-	outFile = fopen("outFile.txt", "w");
+	if (argc == 4) {
+		switch (argv[1][0])
+		{
+			case 'e' : { mode = 1; break; }
+			case 'd' : { mode = 0; break; }
+			default: 
+			{
+				printf("Bad arguments.\n");
+				printf("Usage: enc <e,d> <inFile> <outFile>\n");
+				return 1; 
+			}
+		}
+		inFile = fopen(argv[2], "r");
+		outFile = fopen(argv[3], "w");
+	}
+	else if (argc > 4) {
+		printf("Too many arguments supplied.\n");
+		return 1;
+	}
+	else {
+		printf("Not enough arguments.\n");
+		printf("Usage: enc <e,d> <inFile> <outFile>\n");
+		return 1;
+	}
 
 	if (mode == 1)
 	{
@@ -24,13 +48,22 @@ int main()
 		{
 			character = fgetc(inFile);
 			if (character == EOF) { break; }
-			character = character + seed;
 
-			if (character > 127)
-			{
-				character = 0 + (character - 127);
+			if (character == LF || character == HT) {
+				character = character;
+			}
+			else {
+				character = character + seed;
 			}
 
+			if (character > 126)
+			{
+				character = character - 126;
+			}
+
+			if (character == LF || character == HT) {
+				character = character;
+			}
 			fputc(character, outFile);
 		}
 
@@ -42,12 +75,21 @@ int main()
 			character = fgetc(inFile);
 			if (character == EOF) { break; }
 
-			character = character - seed;
-			if (character < 0)
-			{
+			if (character == LF || character == HT) {
+				character = character;
+			}
+			else {
+
+				character = character - seed;
+			}
+			
+			if (character < 0) {
 				character = character + 2 * character;
 			}
 
+			if (character == LF || character == HT) {
+				character = character;
+			}
 			fputc(character, outFile);
 		}
 		return 0;
